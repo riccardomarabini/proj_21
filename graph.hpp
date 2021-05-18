11328a7fd9f1d78e1b239076df9432ec80ca2a7f
federica-sibani <83833917+federica-sibani@users.noreply.github.com>
2021-05-18 21:04:50 +0200

    adding coloured legend and axes
    Dopo ovviamente bisogna cambiare la riga del main in cui si inserisce il testo per la legenda;
    per gli assi, controllate se ha senso come ho cambiato il calcolo delle x e y dei valori di S,I,R, aggiungendo margin che non sono molto sicura abbia senso

--- graph.hpp
+++ graph.hpp
@@ -62,43 +62,88 @@ class Graph {
   void draw_dots(std::vector<Contagion::State> const& states) {
     const int duration = states.size();
     const int population = states[0].S + states[0].I + states[0].R;
+    const int margin = 20.f;
+    
+    sf::RectangleShape x_axes(sf::Vector2f(window.getSize().x ,2.f));
+    x_axes.setPosition(sf::Vector2f(10, window.getSize().y-margin));
+    x_axes.setFillColor(sf::Color::Black);
+    
+    sf::RectangleShape y_axes(sf::Vector2f(2.f , window.getSize().y));
+    y_axes.setPosition(sf::Vector2f(margin , 0));
+    y_axes.setFillColor(sf::Color::Black);
+    
+     window.draw(x_axes);
+    window.draw(y_axes);
+    
     sf::RectangleShape rect(sf::Vector2f(2.f, 2.f));
 
     for (int i = 0; i != duration; ++i) {
-      float x = (i / static_cast<float>(duration)) * window.getSize().x;
+      float x = margin + (i / static_cast<float>(duration)) * (window.getSize().x - margin);
       rect.setFillColor(sf::Color::Black);
       float y_S = (1 - states[i].S / static_cast<float>(population)) *
-                  window.getSize().y;
+                  (window.getSize().y - margin );
       rect.setPosition(sf::Vector2f(x, y_S));
       window.draw(rect);
       rect.setFillColor(sf::Color::Red);
       float y_I = (1 - states[i].I / static_cast<float>(population)) *
-                  window.getSize().y;
+                  (window.getSize().y- margin );
       rect.setPosition(sf::Vector2f(x, y_I));
       window.draw(rect);
       rect.setFillColor(sf::Color::Green);
       float y_R = (1 - states[i].R / static_cast<float>(population)) *
-                  window.getSize().y;
+                  (window.getSize().y - margin );
       rect.setPosition(sf::Vector2f(x, y_R));
       window.draw(rect);
     }
   }
 
   // funzione che disegna la legenda
-  void write_leg(std::string const& legend) {
+  void write_leg(std::string const& legend,std::string const& nS,std::string const& nI,std::string const& nR) {
+    float const x = window.getSize().x * 0.8;
+    float const y = window.getSize().y * 0.3;
+    
     sf::Text text;
     text.setFont(font);
     text.setString(legend);
-
-    float const x = window.getSize().x * 0.8;
-    float const y = window.getSize().y * 0.1;
     text.setPosition(sf::Vector2f(x, y));
-
     text.setFillColor(sf::Color::Black);
     text.setCharacterSize(15);
-    window.draw(text);
-  }
 
+    sf::Text nStext;
+    nStext.setFont(font);
+    nStext.setString(nS);
+    nStext.setPosition(sf::Vector2f(x, y+16));
+    nStext.setFillColor(sf::Color::Blue);
+    nStext.setCharacterSize(15);
+
+    sf::Text nItext;
+    
+    nItext.setFont(font);
+    nItext.setString(nI);
+    nItext.setPosition(sf::Vector2f(x, y+32));
+    nItext.setFillColor(sf::Color::Red);
+    nItext.setCharacterSize(15);
+
+    sf::Text nRtext;
+    nRtext.setFont(font);
+    nRtext.setString(nR);
+    nRtext.setPosition(sf::Vector2f(x, y+48));
+    nRtext.setFillColor(sf::Color::Green);
+    nRtext.setCharacterSize(15);
+
+    sf::RectangleShape shape(sf::Vector2f(100.f,100.f));
+    shape.setFillColor(sf::Color::Transparent);
+    shape.setOutlineColor(sf::Color::Black);
+    shape.setOutlineThickness(3.f);
+    shape.setPosition(sf::Vector2f(x-10,y-10));
+    
+    window.draw(text);
+    window.draw(nStext);
+    window.draw(nItext);
+    window.draw(nRtext);
+    window.draw(shape);
+  }
+  
   void draw(Environment const& env) {
     float const rect_dim = window.getSize().x / env.side();
     sf::RectangleShape rect(sf::Vector2f(rect_dim, rect_dim));
